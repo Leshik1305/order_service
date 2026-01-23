2dd9dad964dbe9fa6af68023e015dca650e4383d
Leshik1305 <realleshik1305@gmail.com>
2026-01-23 13:44:15 +0300

    fix base_url

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,5 @@
 import os
+from typing import Optional
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -6,8 +7,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     APP_PORT: int
     POSTGRES_CONNECTION_STRING: str
-    BASE_URL: str
-    API_KEY: str
+    BASE_URL: Optional[str] = "http://localhost"
+    API_KEY: Optional[str] = "default_key"
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(__file__), "..", ".env"),
         env_file_encoding="utf-8",
