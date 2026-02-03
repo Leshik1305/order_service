7adaafd30594da5e36a91d3317781f7a73c0a325
Leshik1305 <realleshik1305@gmail.com>
2026-02-03 21:24:52 +0300

    fix

--- src/domain/value_objects/event_type.py
+++ src/domain/value_objects/event_type.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 
 class EventTypeEnum(str, Enum):
-    ORDER_CREATED = "order.created"
+    ORDER_NEW = "order.new"
     ORDER_SHIPPED = "order.shipped"
     ORDER_PAID = "order.paid"
     ORDER_CANCELLED = "order.cancelled"
